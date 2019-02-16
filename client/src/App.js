c7ecba7cd5c7fd40e8d4592a3cd97009d5ad6d24
Marcus Correa <mrc.correa@gmail.com>
2019-02-16 13:07:30 -0200

    separated stateLink in a new file.

--- client/src/App.js
+++ client/src/App.js
@@ -2,65 +2,18 @@ import React, { Component } from "react";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { withClientState } from "apollo-link-state";
 import { ApolloLink } from "apollo-link";
 import Launches from "./components/Launches";
-import gql from "graphql-tag";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import "./App.css";
 import logo from "./logo.png";
+import stateLink from './stateLink'
 
 const cache = new InMemoryCache();
 
-const defaultState = {
-  currentGame: {
-    __typename: "currentGame",
-    teamAScore: 0,
-    teamBScore: 0,
-    teamAName: "Team A",
-    teamBName: "Team B"
-  }
-};
-
-const stateLink = withClientState({
-  cache,
-  defaults: defaultState,
-  resolvers: {
-    Mutation: {
-      updateGame: (_, { index, value }, { cache }) => {
-        const query = gql`
-          query GetCurrentGame {
-            currentGame @client {
-              __typename
-              teamAScore
-              teamBScore
-              teamAName
-              teamBName
-            }
-          }
-        `;
-        const previousState = cache.readQuery({ query });
-
-        const data = {
-          ...previousState,
-          currentGame: {
-            ...previousState.currentGame,
-            [index]: value
-          }
-        };
-
-        console.log('DATA', data)
-
-        cache.writeData({query, data})
-        return data
-      }
-    }
-  }
-});
-
 const client = new ApolloClient({
   link: ApolloLink.from([
-    stateLink,
+    stateLink(cache),
     new HttpLink({
       uri: "https://core-graphql.dev.waldo.photos/pizza"
     })
